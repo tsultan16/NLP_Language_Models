a921d0cbc9e91af1f72b518006136755eee9b0c9
tsultan16 <tanzids@student.unimelb.edu.au>
2023-11-28 15:21:16 +1100

    implemented london baseline

--- Assignment5/src/london_baseline.py
+++ Assignment5/src/london_baseline.py
@@ -2,3 +2,14 @@
 #   example in the dev set.
 # Hint: Make use of existing code.
 # Your solution here should only be a few lines.
+
+# read devset name data from file
+correct = 0
+total = 0
+for line in open('birth_dev.tsv', encoding='utf-8'):
+    place = line.split('\t')[1].strip("\n")
+    if place == 'London':
+        correct += 1
+    total += 1                
+            
+print(f"Devset London-Baseline, Num Correct: {correct}, Total: {total}, Accuracy: {correct/total}")
