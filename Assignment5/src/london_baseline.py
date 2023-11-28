214b20f318b2113d1aa7aeda3fa6afaef2eef462
tsultan16 <tanzids@student.unimelb.edu.au>
2023-11-28 15:27:24 +1100

    implemented london baseline

--- Assignment5/src/london_baseline.py
+++ Assignment5/src/london_baseline.py
@@ -3,13 +3,24 @@
 # Hint: Make use of existing code.
 # Your solution here should only be a few lines.
 
+from collections import Counter
+import pprint
+
+pp = pprint.PrettyPrinter()
+
+
 # read devset name data from file
 correct = 0
 total = 0
+places = []
 for line in open('birth_dev.tsv', encoding='utf-8'):
     place = line.split('\t')[1].strip("\n")
+    places.append(place)
     if place == 'London':
         correct += 1
     total += 1                
-            
+
 print(f"Devset London-Baseline, Num Correct: {correct}, Total: {total}, Accuracy: {correct/total}")
+
+counter = Counter(places)
+pp.pprint(counter.most_common())
