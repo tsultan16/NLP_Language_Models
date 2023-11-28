a67f9850159102ef0c5be8c4c869f8b1526438b7
tsultan16 <tanzids@student.unimelb.edu.au>
2023-11-28 17:33:10 +1100

    pretraining and finetuning completed

--- Assignment5/src/run.py
+++ Assignment5/src/run.py
@@ -112,8 +112,6 @@ if args.function == 'pretrain':
     # save the trained model
     torch.save(model.state_dict(), args.writing_params_path)
 
-
-    raise NotImplementedError
 elif args.function == 'finetune':
     assert args.writing_params_path is not None
     assert args.finetune_corpus_path is not None
@@ -182,7 +180,6 @@ elif args.function == 'finetune':
  
     # save the trained model
     torch.save(model.state_dict(), args.writing_params_path)
-
 
 elif args.function == 'evaluate':
     assert args.outputs_path is not None
